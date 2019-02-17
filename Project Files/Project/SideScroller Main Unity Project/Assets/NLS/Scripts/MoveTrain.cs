c3cf4564d301ae137bcb815bf2fb49856f46a5a9
Nathen Toms <45152858+NTomsNC@users.noreply.github.com>
2019-02-16 21:52:40 -0500

    Placholder Movetrain scripts
    The script is sketchley bound to Q and E at all times

---
+++ Project Files/Project/SideScroller Main Unity Project/Assets/NLS/Scripts/MoveTrain.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTrain : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    private float origin;
+
+    public float speed = 0.1f;
+    public float maxDis = 0;
+    public float minDis = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        targetPosition = this.gameObject.transform.position;
+        origin = this.gameObject.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+            if (this.gameObject.transform.position.x <= origin + maxDis)
+                targetPosition += new Vector3(speed, 0, 0);
+
+        if (Input.GetKey(KeyCode.Q))
+            if (this.gameObject.transform.position.x >= origin + minDis)
+                targetPosition += new Vector3(-speed, 0, 0);
+
+        this.gameObject.transform.position = Vector3.Lerp(this.gameObject.transform.position, targetPosition, speed);
+    }
+}
