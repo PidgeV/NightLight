4cdd58e2f8501ef6f5d1c469c93d7f117857234b
houghtob123 <houghtob123@gmail.com>
2019-02-20 21:42:21 -0500

    will this work?
    I hope so

---
+++ Project Files/Project/SideScroller Main Unity Project/Assets/NLS/Scripts/Lights/FlashingLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashingLight : MonoBehaviour {
+    public Vector2 delayRange = new Vector2(1,2);
+    Light light;
+	// Use this for initialization
+	void Start () {
+        light = GetComponent<Light>();
+        StartCoroutine(LightFlash());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator LightFlash()
+    {
+        while(true)
+        {
+            light.enabled = !light.enabled;
+            yield return new WaitForSeconds(Random.Range(delayRange.x, delayRange.y));
+        }
+    }
+}
