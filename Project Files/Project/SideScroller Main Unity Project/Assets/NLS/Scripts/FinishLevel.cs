b865285f35841c0e841f906f6f4f9429bed826fb
PidgeV <39803542+PidgeV@users.noreply.github.com>
2019-03-05 19:22:46 -0500

    Main Menu
    Added level load script

--- Project Files/Project/SideScroller Main Unity Project/Assets/NLS/Scripts/FinishLevel.cs
+++ Project Files/Project/SideScroller Main Unity Project/Assets/NLS/Scripts/FinishLevel.cs
@@ -9,7 +9,10 @@ public class FinishLevel : MonoBehaviour {
     public bool lastLevel = false;
     public float loadTimer = 0;
     [Header("This is the index for the scene you wish to load, see File->Build Settings")]
+    [Header("If wishing for 1 scene, set all 3 to the same value")]
     public int sceneIndex = 0;
+    public int maxIndex = 0;
+    public int minIndex = 0;
     [Header("Amount of time(in seconds) player must stand in trigger to reload the level.")]
     public float timeBeforeLoad = 1;
 
@@ -20,9 +23,18 @@ public class FinishLevel : MonoBehaviour {
         //When the timer reaches the set delay time, load the specefied scene.
         if (loadTimer >= timeBeforeLoad)
         {
+            lastLevel = sceneIndex == maxIndex ? true : false;
+
             if (other.tag == "Player" && !lastLevel)
             {
-                SceneManager.LoadScene(sceneIndex);
+                try
+                {
+                    SceneManager.LoadScene(sceneIndex);
+                }
+                catch (System.Exception e)
+                {
+                    Debug.Log(e.Message);
+                }
             }
             else if (other.tag == "Player" && lastLevel)
             {
@@ -30,6 +42,30 @@ public class FinishLevel : MonoBehaviour {
             }
 
         }
+    }
+
+    public void GoToLevel()
+    {
+        try
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+    }
+    public void IncIndex()
+    {
+        sceneIndex++;
+        if (sceneIndex > maxIndex)
+            sceneIndex = minIndex;
+    }
+    public void DecIndex()
+    {
+        sceneIndex--;
+        if (sceneIndex < minIndex)
+            sceneIndex = maxIndex;
     }
 
 }
