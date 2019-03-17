367e09716ab0497e44ee0a91762c17e2f7876b25
houghtob123 <houghtob123@gmail.com>
2019-03-16 21:05:27 -0400

    Fen particle fade
    -Added script to fens particle system that fades it based on health

---
+++ Project Files/Project/SideScroller Main Unity Project/Assets/FadeParticlesWithHealth.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class FadeParticlesWithHealth : MonoBehaviour
+{
+    Health parentHealth;
+    float startHP;
+    Material material;
+    // Use this for initialization
+    void Start()
+    {
+        parentHealth = transform.parent.GetComponent<Health>();
+        startHP = parentHealth.currentHealth;
+        material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color temp = material.color;
+        temp.a = parentHealth.currentHealth / startHP;
+        material.color = temp;
+    }
+}
