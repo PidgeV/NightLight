ddc34e0abaf96e3e34cdb4527a4cc8bf5993e98c
PeterJM94 <46722925+PeterJM94@users.noreply.github.com>
2019-03-19 00:01:55 -0400

    Added pause menu
    Created a prefab pause menu and stuck it in both levels. Functional but bugs out if you select to go to the main menu. When going to the menu selection screen for the second time the spin functionality stops working and in rare cases loads the level wrong.

---
+++ Project Files/Project/SideScroller Main Unity Project/Assets/NLS/Scripts/Interactions/HighlightText.cs
@@ -0,0 +1,62 @@
+﻿//---------------------------------------------------------
+//-- Peter Matesic
+//-- Used as a replacement for a button to implement a 
+//-- glow effect as well as button funcionality
+//---------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HighlightText : Selectable, IPointerEnterHandler, ISelectHandler  
+{
+    public GameObject levelControl;
+    public GameObject defaultText;
+    public GameObject highLightText;
+    private BaseEventData m_BaseEvent;
+    public enum ButtonEvent { PLAY, MAIN_MENU, QUIT};
+    public ButtonEvent pauseEvent;
+
+    void Update()
+    {
+        if (IsHighlighted(m_BaseEvent) == true)
+        {
+            defaultText.SetActive(false);
+            highLightText.SetActive(true);
+        }
+        else if(IsPressed() == true)
+        {
+            if (pauseEvent == ButtonEvent.PLAY)
+            {
+                defaultText.SetActive(true);
+                highLightText.SetActive(false);
+                levelControl.GetComponent<PauseMenu>().hidePaused();
+            }
+            else if(pauseEvent == ButtonEvent.MAIN_MENU)
+            {
+                try
+                {
+                    SceneManager.LoadScene(0);
+                }
+                catch (System.Exception e)
+                {
+                    Debug.Log(e.Message);
+                }
+            }
+            else if(pauseEvent == ButtonEvent.QUIT)
+            {
+                Application.Quit();
+            }
+        }
+        else
+        {
+            defaultText.SetActive(true);
+            highLightText.SetActive(false);
+        }
+    }    
+}
---
+++ Project Files/Project/SideScroller Main Unity Project/Assets/NLS/Scripts/Interactions/PauseMenu.cs
@@ -0,0 +1,69 @@
+﻿//---------------------------------------------------------
+//-- Peter Matesic
+//-- Used for revealing / hiding the pause menu
+//---------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    GameObject[] pauseObjects;
+    //public GameObject postProcessGameObject; // To add a blurr effect
+    public bool isPaused;
+
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 1;
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        hidePaused();
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //uses the Escape button to pause and unpause the game
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                hidePaused();
+            }
+            else
+            {
+                showPaused();
+            }
+        }
+    }
+
+    //shows objects with ShowOnPause tag
+    public void showPaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+        isPaused = true;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        //postProcessGameObject.SetActive(true);
+    }
+
+    //hides objects with ShowOnPause tag
+    public void hidePaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+        isPaused = false;
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+}
+
