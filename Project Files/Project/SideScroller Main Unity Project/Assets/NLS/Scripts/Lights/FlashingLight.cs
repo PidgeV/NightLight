767c083b1ebd363d39719df80d91f3cf201a397c
Nathen Toms <45152858+NTomsNC@users.noreply.github.com>
2019-03-20 00:04:07 -0400

    Lvl 1 Done , just needs playtesting

--- Project Files/Project/SideScroller Main Unity Project/Assets/NLS/Scripts/Lights/FlashingLight.cs
+++ Project Files/Project/SideScroller Main Unity Project/Assets/NLS/Scripts/Lights/FlashingLight.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlashingLight : MonoBehaviour {
+public class FlashingLight : MonoBehaviour
+{
     public Vector2 delayRange = new Vector2(1,2);
+    public bool startState = false;
+
+    private bool _Enabled = false;
+    private bool firstUpdate = true;
     Light light;
+
 	// Use this for initialization
 	void Start () {
         light = GetComponent<Light>();
+        light.enabled = !startState;
         StartCoroutine(LightFlash());
 	}
 	
@@ -16,12 +23,26 @@ public class FlashingLight : MonoBehaviour {
 		
 	}
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (firstUpdate)
+        {
+            _Enabled = true;
+            light.enabled = startState;
+            firstUpdate = false;
+        }
+    }
+
     IEnumerator LightFlash()
     {
         while(true)
         {
-            light.enabled = !light.enabled;
             yield return new WaitForSeconds(Random.Range(delayRange.x, delayRange.y));
+
+            if (_Enabled)
+            {
+                light.enabled = !light.enabled;
+            }
         }
     }
 }
