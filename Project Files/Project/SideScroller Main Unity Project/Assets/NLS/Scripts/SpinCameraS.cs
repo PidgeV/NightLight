7b3d08f13a9ed71bedd7e3b1aaf472a72d6c3422
Nathen Toms <45152858+NTomsNC@users.noreply.github.com>
2019-03-31 18:27:34 -0400

    pray

---
+++ Project Files/Project/SideScroller Main Unity Project/Assets/NLS/Scripts/SpinCameraS.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinCameraS : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+
+    [Range(0.01f, 1.0f)]
+    public float smooth = 0.5f;
+
+    public bool LookAtTarget = false;
+    public bool RotateArountTarget = true;
+
+    public float RotationSpeed = 5.0f;
+
+    private bool update = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (RotateArountTarget)
+            {
+                Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationSpeed, Vector3.up);
+
+                offset = camTurnAngle * offset;
+            }
+            Vector3 newPos = target.position + offset;
+            transform.position = Vector3.Slerp(transform.position, newPos, smooth);
+
+            if (LookAtTarget || RotateArountTarget)
+                transform.LookAt(target);
+        }
+    }
+}
