caaaa5d7be2e3bb4f8b9d13c1305c99d7f4d1d14
houghtob123 <houghtob123@gmail.com>
2019-04-01 21:29:03 -0400

    Level update and screenshot button implemented

---
+++ Project Files/Project/SideScroller Main Unity Project/Assets/NLS/Scripts/CameraScreenShot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class CameraScreenShot : MonoBehaviour {
+
+    public void ScreenShot()
+    {
+        ScreenCapture.CaptureScreenshot("Screenshot.jpg");
+    }
+}
+
+//---------------------------------------------------------------------------
+//Custom inspector
+#if UNITY_EDITOR
+[CustomEditor(typeof(CameraScreenShot))]
+public class CameraScreenShot_Editor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        CameraScreenShot script = target as CameraScreenShot;
+
+        if (GUILayout.Button("Take ScreenShot"))
+        {
+            script.ScreenShot();
+        }
+    }
+}
+#endif
